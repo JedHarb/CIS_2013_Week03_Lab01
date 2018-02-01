4606da1390d6d5486a6b6eeda75e0e969e4ace9c
JedHarb <jedharb@gmail.com>
2018-01-31 20:26:22 -0600

    Week 3 in class work updated
    Week 3 in class work updated

--- flowControl.cpp
+++ flowControl.cpp
@@ -5,24 +5,27 @@ int main()
 {
 	char continueProgram = 'a';
 
-
-	while (continueProgram == 'y') {
-		cout << "Do you want to continue (y/n)?";
-		cin >> continueProgram;
-
-		switch (continueProgram) {
-		case 'y':
-			cout << "Going to continue" << endl;
-			break;
-
-		case 'n':
-			cout << "Going to exit" << endl;
-			break;
-
-		default:
-			continueProgram = 'y';
-		}
+	for (int i = 99; i >= 0; i = i - 1) {
+		cout << i << " bottles of beer on the wall" << endl;
 	}
+
+	//while (continueProgram == 'y') {
+	//	cout << "Do you want to continue (y/n)?";
+	cin >> continueProgram;
+
+	//	switch (continueProgram) {
+	//	case 'y':
+	//		cout << "Going to continue" << endl;
+	//		break;
+
+	//	case 'n':
+	//		cout << "Going to exit" << endl;
+	//		break;
+
+	//	default:
+	//		continueProgram = 'y';
+	//	}
+	//}
 
 	return 0;
 }
