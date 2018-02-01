0da9da7f661d3b43f1b21c810a8de8d4dbfd9572
JedHarb <jedharb@gmail.com>
2018-01-31 20:14:28 -0600

    Classwork Week 03
    Classwork Week 03

---
+++ flowControl.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char continueProgram = 'a';
+
+
+	while (continueProgram == 'y') {
+		cout << "Do you want to continue (y/n)?";
+		cin >> continueProgram;
+
+		switch (continueProgram) {
+		case 'y':
+			cout << "Going to continue" << endl;
+			break;
+
+		case 'n':
+			cout << "Going to exit" << endl;
+			break;
+
+		default:
+			continueProgram = 'y';
+		}
+	}
+
+	return 0;
+}
